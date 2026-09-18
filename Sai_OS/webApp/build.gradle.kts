0ac9a1e583143b6cbc0aa347988770294572acc0
p siva sai <56314427+pallasivasai@users.noreply.github.com>
2026-09-18 08:38:41 +0530

    Add SAI Voice OS web module

---
+++ Sai_OS/webApp/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("multiplatform")
+    id("org.jetbrains.compose")
+    id("org.jetbrains.kotlin.plugin.compose")
+}
+
+kotlin {
+    @OptIn(ExperimentalWasmDsl::class)
+    wasmJs {
+        browser()
+        binaries.executable()
+    }
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(compose.runtime)
+            implementation(compose.foundation)
+            implementation(compose.material3)
+            implementation(compose.ui)
+        }
+    }
+}
