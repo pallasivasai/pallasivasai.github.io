d0c31f8165d29c4910e8c6a076d0e2df20e62ba8
p siva sai <56314427+pallasivasai@users.noreply.github.com>
2026-09-18 08:41:12 +0530

    Connect ComposeViewport to webApp container

--- Sai_OS/webApp/src/wasmJsMain/kotlin/main.kt
+++ Sai_OS/webApp/src/wasmJsMain/kotlin/main.kt
@@ -3,7 +3,7 @@ import androidx.compose.ui.window.ComposeViewport
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
-    ComposeViewport {
+    ComposeViewport(viewportContainerId = "webApp") {
         App()
     }
 }
